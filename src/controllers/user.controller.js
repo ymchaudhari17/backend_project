72a17096184bfccea9c37f5819fdf05516f3f4c5
Yagmi <ymchaudhari17@gmail.com>
2026-06-11 11:03:50 +0530

    refactor response format in changeCurrentPassword and getCurrentUser functions to use ApiResponse

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -190,12 +190,12 @@ const changeCurrentPassword=asyncHandler(async(req,res)=>{
  user.password=new password
 await user.save({validateBeforeSave:false})
 return res.status(200)
-.json(200,{},"Password changed succesfully")
+.json(new ApiResponse(200,{},"Password changed succesfully"))
 })
 
 const getCurrentUser=asyncHandler(async(req,res)=>{
   return res.status(200)
-  .json(200,req.user,"current user fetched succesfully")
+  .json(new ApiResponse(200,req.user,"current user fetched succesfully"))
 })
 
 const updateAccountDetails=asyncHandler(async(req,res)=>{
