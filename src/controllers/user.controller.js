d94c6ba76d0ec83fc394ebc5772039dc1aaa41d2
Yagmi <ymchaudhari17@gmail.com>
2026-06-18 19:49:32 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -335,8 +335,60 @@ return res.status(200)
 )
 });
 
+const getWatchHistory=asyncHandler(async(req,res)=>{
+ const user=await User.aggregate([
+  {
+    $match:{
+      _id:new mongoose.Types.ObjectId(req.user._id)
+    }
+  },
+  {
+   $lookup:{
+    from:"videos",
+    localField:"watchHistory",
+    foreigField :"_id",
+    as:"watchHistory",
+    pipeline:[
+      {
+        $lookup:{
+          form:"users",
+          localField:"owner",
+          foreignField:"_ id",
+          as:"owner",
+          pipeline:[
+            {
+              $project:{
+                fullname:1,
+                username:1,
+                avatar:1
+              }
+            }
+          ]
+        }
+      },
+      {
+        $addFields:{
+          owner:{
+            $first:"$owner"
+          }
+        }
+      }
+    ]
+   }
+  }
+ ])
+ return res.status(200)
+ .json(
+  new ApiResponse(
+    200,
+    user[0].watchHistory,
+    "watch history fetched succesfully"
+  )
+ )
+})
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
  //get user from frontend
    //validation
    //check if user is already registered- using email or username
